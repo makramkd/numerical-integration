b1f6a756a0f24a9054e54c5952bf92d28ba37b6d
Makram Kamaleddine <makram.kd@gmail.com>
2015-12-09 11:51:34 -0500

    Calling code and project files

--- src/integrate.hpp
+++ src/integrate.hpp
@@ -7,5 +7,6 @@
 
 // include all integration headers
 #include "rectangle_rule.hpp"
+#include "trapezoidal_rule.hpp"
 
 #endif //SRC_INTEGRATE_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,8 @@ double f(double x) {
 
 int main() {
     auto value = in::rectangle_rule(f, 0.0, 1.0);
+    auto tvalue = in::trapezoidal_rule(f, 0.0, 1.0);
 
-    std::cout << value << std::endl;
+    std::cout << "rectangle rule: " << value << std::endl;
+    std::cout << "trapezoidal rule: " << tvalue << std::endl;
 }
